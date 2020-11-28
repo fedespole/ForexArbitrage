62bb5b946f5e910d379a329491dd1154d4c9f7da
Federico Spoletini <75097675+fedespole@users.noreply.github.com>
2020-11-28 13:51:25 +0100

    Update FA_SampleTest1.py

--- src/FA_SampleTest1.py
+++ src/FA_SampleTest1.py
@@ -3,13 +3,13 @@ from src import ForexArbitrage
 #v1 = 5
 #tickers1 = ['USD', 'EUR', 'GBP', 'CHF', 'CAD']     # Major Currency Pairs (Us Dollar, Euro, Pound Sterling, Swiss Franc, Canadian Dollar)
 
-#g = Arbitrage.Graph(v1, tickers1)
+#g = ForexArbitrage.Graph(v1, tickers1)
 #g.Bellman_Ford()
 
 #v2 = 4
 #tickers2 = ['TRY', 'MXN', 'HUF', 'THB']    # Exotic Currency Pairs (Turkish Lira, Mexican Peso, Hungarian Forint, Thai Baht)
 
-#g = Arbitrage.Graph(v2, tickers2)
+#g = ForexArbitrage.Graph(v2, tickers2)
 #g.Bellman_Ford()
 
 v3 = 6
